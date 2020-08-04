2486d4b3694ea73af95a0a5fc8e5ae357236b219
Giang <jun154607@gmail.com>
2020-08-04 15:17:39 +0700

    ###

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
 Route::get('/welcome', function () {
     return view('welcome');
 });
-Route::resources('home','HomeController');
+Route::get('home','HomeController@index');
