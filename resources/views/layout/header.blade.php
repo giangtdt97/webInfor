0eb71aaa15c442f1e8964404406b82d5f06ddc12
Giang <jun154607@gmail.com>
2020-08-05 16:04:10 +0700

    ##

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -29,7 +29,11 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-
+<style>
+    a:hover {
+        background-color:#b0d4f1;
+    }
+</style>
 
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
     <div class="container">
@@ -40,7 +44,7 @@
 
         <div class="collapse navbar-collapse" id="ftco-nav">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item active"><a href="{{route('home')}} "class="nav-link">Home</a></li>
+                <li class="nav-item active"><a href="{{route('home')}} "class="nav-link" >Home</a></li>
                 <li class="nav-item"><a href="{{route('about')}}" class="nav-link">About</a></li>
                 <li class="nav-item"><a href="{{route('services')}}" class="nav-link">Services</a></li>
                 <!--	          <li class="nav-item"><a class="nav-link" href="">Work</a></li>-->
