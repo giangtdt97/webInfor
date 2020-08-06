fcadf8fe5b3b8140cb2ada401cc165a385c1c05a
Giang <jun154607@gmail.com>
2020-08-06 14:40:28 +0700

    update page about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,7 +9,7 @@
                     <div class="row">
                         <div class="col-md-12 nav-link-wrap mb-5">
                             <div class="nav ftco-animate nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                                <a class="nav-link active" id="v-pills-whatwedo-tab" data-toggle="pill" href="#v-pills-whatwedo" role="tab" aria-controls="v-pills-whatwedo" aria-selected="true">What we do</a>
+                                <a class="nav-link" id="v-pills-whatwedo-tab" data-toggle="pill" href="#v-pills-whatwedo" role="tab" aria-controls="v-pills-whatwedo" aria-selected="true">What we do</a>
 
                                 <a class="nav-link" id="v-pills-mission-tab" data-toggle="pill" href="#v-pills-mission" role="tab" aria-controls="v-pills-mission" aria-selected="false">Our mission</a>
 
@@ -20,7 +20,7 @@
 
                             <div class="tab-content ftco-animate" id="v-pills-tabContent">
 
-                                <div class="tab-pane fade show active" id="v-pills-whatwedo" role="tabpanel" aria-labelledby="v-pills-whatwedo-tab">
+                                <div class="tab-pane show active" id="v-pills-whatwedo" role="tabpanel" aria-labelledby="v-pills-whatwedo-tab">
                                     <div>
                                         <h2 class="mb-4">We Offer High Quality Services</h2>
                                         <p>BANABA Tech Co., LTD is a Vietnam firm that specializes in software development and outsourcing especially in mobile APP (Android and IOS).&nbsp; The company has offered a wide range of high quality services in the development, delivery, and maintenance of software in Vietnam.</p>
